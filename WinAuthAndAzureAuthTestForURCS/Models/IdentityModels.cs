1c1bf219b8e099e5e4a4776caff88af17690059f
Luke Hammer <Luke@lukehammermagic.com>
2017-08-25 14:12:51 -0700

    add idenity

---
+++ WinAuthAndAzureAuthTestForURCS/Models/IdentityModels.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace WinAuthAndAzureAuthTestForURCS.Models
+{
+	public class IdentityModels
+	{
+	    public class ApplicationUser : IdentityUser
+	    {
+	        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+	        {
+	            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+	            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+	            // Add custom user claims here
+	            return userIdentity;
+	        }
+	    }
+
+	    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+	    {
+	        public ApplicationDbContext()
+	            : base("DefaultConnection", throwIfV1Schema: false)
+	        {
+	        }
+
+	        public static ApplicationDbContext Create()
+	        {
+	            return new ApplicationDbContext();
+	        }
+	    }
+    }
+}
